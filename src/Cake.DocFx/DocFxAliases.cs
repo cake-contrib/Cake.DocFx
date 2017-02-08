0b928685bf4937844fcdfd39afae8d5bde95494b
Pascal Berger <pascal.berger@gmail.com>
2017-02-08 13:57:55 +0100

    Add support for running docfx init

--- src/Cake.DocFx/DocFxAliases.cs
+++ src/Cake.DocFx/DocFxAliases.cs
@@ -247,5 +247,53 @@ namespace Cake.DocFx
             var runner = new DocFxMetadataRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
             runner.Run(settings);
         }
+
+        /// <summary>
+        /// Generate an initial <code>docfx.json</code> file.
+        /// </summary>
+        /// <param name="context">The Cake context.</param>
+        /// <example>
+        /// <code>
+        /// DocFxInit();
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Documentation")]
+        public static void DocFxInit(this ICakeContext context)
+            => context.DocFxInit((DocFxInitSettings)null);
+
+        /// <summary>
+        /// Generate an initial <code>docfx.json</code> file, with the specified settings.
+        /// </summary>
+        /// <param name="context">The Cake context.</param>
+        /// <param name="settings">The DocFx settings.</param>
+        /// <example>
+        /// <code>
+        /// DocFxInit(new DocFxInitSettings()
+        /// {
+        ///     OutputPath = "./docs"
+        /// });
+        /// </code>
+        /// </example>
+        /// <example>
+        /// <code>
+        /// DocFxInit(new DocFxInitSettings()
+        /// {
+        ///     OutputPath = "./docs",
+        ///     OnlyConfigFile = true
+        /// });
+        /// </code>
+        /// </example>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Documentation")]
+        public static void DocFxInit(this ICakeContext context, DocFxInitSettings settings)
+        {
+            Contract.NotNull(context, nameof(context));
+
+            settings = settings ?? new DocFxInitSettings();
+
+            var runner = new DocFxInitRunner(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            runner.Run(settings);
+        }
     }
 }
---
+++ src/Cake.DocFx/DocFxInitSettings.cs
@@ -0,0 +1,23 @@
+﻿using Cake.Core.IO;
+using Cake.Core.Tooling;
+
+namespace Cake.DocFx
+{
+    /// <summary>
+    /// Contains settings used by <see cref="DocFxInitRunner"/>.
+    /// </summary>
+    public class DocFxInitSettings : ToolSettings
+    {
+        /// <summary>
+        /// Gets or sets a flag indicating if only the <code>docfx.json</code> file should be generated.
+        /// In this case no project folder will be generated.
+        /// </summary>
+        public bool OnlyConfigFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the output folder of the config file.
+        /// If not specified, the config file will be saved to a new folder <code>docfx_project</code>.
+        /// </summary>
+        public DirectoryPath OutputPath { get; set; }
+    }
+}
