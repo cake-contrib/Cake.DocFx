47c51fed0de002dcff81f96efcc642bfb4681e7a
Gary Ewan Park <gep13@gep13.co.uk>
2017-02-15 20:12:15 +0000

    AppVeyor Publish: 5c51f4040efba28e353eabdf504e88faa4bec985 (build) Corrected hint paths - again

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,154 @@
+
+var camelCaseTokenizer = function (obj) {
+    var previous = '';
+    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+        var current = cur.toLowerCase();
+        if(acc.length === 0) {
+            previous = current;
+            return acc.concat(current);
+        }
+        previous = previous.concat(current);
+        return acc.concat([current, previous]);
+    }, []);
+}
+lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+        this.tokenizer(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"DocFxSettings",
+        content:"DocFxSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
+        title:"DocFxMetadataRunner",
+        content:"DocFxMetadataRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:2,
+        title:"DocFxMetadataSettings",
+        content:"DocFxMetadataSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"DocFxRunner",
+        content:"DocFxRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:4,
+        title:"DocFxAliases",
+        content:"DocFxAliases",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:5,
+        title:"DocFxTool",
+        content:"DocFxTool",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:6,
+        title:"DocFxBuildRunner",
+        content:"DocFxBuildRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:7,
+        title:"DocFxBuildSettings",
+        content:"DocFxBuildSettings",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxSettings',
+        title:"DocFxSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxMetadataRunner',
+        title:"DocFxMetadataRunner",
+        description:""
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxMetadataSettings',
+        title:"DocFxMetadataSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxRunner',
+        title:"DocFxRunner",
+        description:""
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxAliases',
+        title:"DocFxAliases",
+        description:""
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxTool_1',
+        title:"DocFxTool<TSettings>",
+        description:""
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxBuildRunner',
+        title:"DocFxBuildRunner",
+        description:""
+    });
+
+    y({
+        url:'/Cake.DocFx/Cake.DocFx/api/Cake.DocFx/DocFxBuildSettings',
+        title:"DocFxBuildSettings",
+        description:""
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
