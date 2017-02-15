bb4567bef2ed39140a254d6b9028e70dd0b7ca11
Gary Ewan Park <gep13@gep13.co.uk>
2017-02-15 13:50:54 +0000

    (GH-19) Removed usage of obsolete code

--- src/Cake.DocFx/DocFxBuildRunner.cs
+++ src/Cake.DocFx/DocFxBuildRunner.cs
@@ -1,5 +1,6 @@
 ﻿using Cake.Core;
 using Cake.Core.IO;
+using Cake.Core.Tooling;
 using Cake.DocFx.Helper;
 
 namespace Cake.DocFx
@@ -15,9 +16,9 @@ namespace Cake.DocFx
         /// <param name="fileSystem">The file system.</param>
         /// <param name="environment">The environment.</param>
         /// <param name="processRunner">The process runner.</param>
-        /// <param name="globber">The globber.</param>
-        public DocFxBuildRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner, IGlobber globber) 
-            : base(fileSystem, environment, processRunner, globber)
+        /// <param name="tools">The tools.</param>
+        public DocFxBuildRunner(IFileSystem fileSystem, ICakeEnvironment environment, IProcessRunner processRunner, IToolLocator tools) 
+            : base(fileSystem, environment, processRunner, tools)
         {
         }
 
