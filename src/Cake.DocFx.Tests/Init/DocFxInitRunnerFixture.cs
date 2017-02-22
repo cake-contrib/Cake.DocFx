5a158e0ddedd4dc93e9a25579dcf6fc290e38fc5
Pascal Berger <pascal.berger@gmail.com>
2017-02-22 12:33:26 +0100

    Add test cases for docfx init

---
+++ src/Cake.DocFx.Tests/Init/DocFxInitRunnerFixture.cs
@@ -0,0 +1,15 @@
+﻿namespace Cake.DocFx.Tests.Init
+{
+    internal sealed class DocFxInitRunnerFixture : DocFxFixture<DocFxInitSettings>
+    {
+        public DocFxInitRunnerFixture()
+        {
+        }
+
+        protected override void RunTool()
+        {
+            var tool = new DocFxInitRunner(FileSystem, Environment, ProcessRunner, Tools);
+            tool.Run(Settings);
+        }
+    }
+}
---
+++ src/Cake.DocFx.Tests/Init/DocFxInitRunnerTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Cake.DocFx.Tests.Init
+{
+    using Xunit;
+
+    public class DocFxInitRunnerTests
+    {
+        public sealed class ThePackMethod
+        {
+            [Fact]
+            public void Should_Throw_If_Settings_Are_Null()
+            {
+                // Given
+                var fixture = new DocFxInitRunnerFixture();
+                fixture.Settings = null;
+
+                // When
+                var result = Record.Exception(() => fixture.Run());
+
+                // Then
+                result.IsArgumentNullException("settings");
+            }
+
+            [Fact]
+            public void Should_Add_Mandatory_Arguments()
+            {
+                // Given
+                var fixture = new DocFxInitRunnerFixture();
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("init -q", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_OnlyConfigFile_To_Arguments_If_Not_Null()
+            {
+                // Given
+                var fixture = new DocFxInitRunnerFixture();
+                fixture.Settings.OnlyConfigFile = true;
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("init -q -f", result.Args);
+            }
+
+            [Fact]
+            public void Should_Add_OutputPath_To_Arguments_If_Not_Null()
+            {
+                // Given
+                var fixture = new DocFxInitRunnerFixture();
+                fixture.Settings.OutputPath = @"c:\foo bar\";
+
+                // When
+                var result = fixture.Run();
+
+                // Then
+                Assert.Equal("init -q -o \"c:/foo bar\"", result.Args);
+            }
+        }
+    }
+}
